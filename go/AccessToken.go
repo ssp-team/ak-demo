f924c8d5c2cec79811bfc2cb67bb5668006f3fd1
zhaidw <git@zhaidw.com>
2021-01-27 11:28:14 +0800

    fix ts

--- go/AccessToken.go
+++ go/AccessToken.go
@@ -4,22 +4,21 @@ import (
     "fmt"
     "time"
     "strings"
-    "strconv"
     "encoding/base64"
     "github.com/xxtea/xxtea-go/xxtea"
 )
 
 func createAccessToken(accessKeyId string, mediaId string, accessKeySecret string) string{
 
-	ts := strconv.FormatInt(time.Now().Unix() * 1000,10)
-        tempStr := accessKeyId + "|" + string(ts) + "|" + mediaId
-        createAccessToken := xxtea.Encrypt( []byte(tempStr), []byte(accessKeySecret))
+	ts := time.Now().UnixNano() / 1e6
+    tempStr := accessKeyId + "|" + string(ts) + "|" + mediaId
+    createAccessToken := xxtea.Encrypt( []byte(tempStr), []byte(accessKeySecret))
 	var createAccessTokenStr = base64.StdEncoding.EncodeToString(createAccessToken)
-        createAccessTokenStr = strings.Replace(createAccessTokenStr,"+", "-",-1)
-        createAccessTokenStr = strings.Replace(createAccessTokenStr,"/", "_",-1)
-        createAccessTokenStr = strings.Replace(createAccessTokenStr,"=", "*",-1)
+    createAccessTokenStr = strings.Replace(createAccessTokenStr,"+", "-",-1)
+    createAccessTokenStr = strings.Replace(createAccessTokenStr,"/", "_",-1)
+    createAccessTokenStr = strings.Replace(createAccessTokenStr,"=", "*",-1)
 
-        return createAccessTokenStr
+    return createAccessTokenStr
 }
 
 func main() {
@@ -30,6 +29,5 @@ func main() {
     fmt.Println("accessKeyId: ", accessKeyId)
 		
     accessToken := createAccessToken(accessKeyId,mediaId,accessKeySecret)
-
     fmt.Println("accessToken: ", accessToken)
 }
